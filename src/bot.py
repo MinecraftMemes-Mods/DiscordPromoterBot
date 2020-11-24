0cdf924e3109325cafd6f6e6dd488663ad009fb1
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-11-24 19:52:29 +0100

    fixed semantics

--- src/bot.py
+++ src/bot.py
@@ -1,34 +1,36 @@
 import time
-from praw import reddit
+from praw import Reddit
 from os import getenv
 from sql_wrapper import SQL
 from dotenv import load_dotenv
 load_dotenv()
 
-reddit = reddit(
+reddit = Reddit(
     client_id=getenv('DPB_CLIENTID'),
     client_secret=getenv('DPB_CLIENTSECRET'),
-    user_agent=f"{getenv('DPB_SUBREDDIT')}'s DiscordPromoterBot'}",
+    user_agent=f"{getenv('DPB_SUBREDDIT')}'s DiscordPromoterBot'",
     username=getenv('DPB_USERNAME'),
     password=getenv('DPB_PASSWORD'),
 )
 
 db = SQL('posts.db')
 
-submission_stream = reddit.subreddit(os.getenv('SUBREDDIT')).stream.submissions()
+submission_stream = reddit.subreddit(
+    getenv('DPB_SUBREDDIT')).stream.submissions()
 
 while True:
     for submission in submission_stream:
         # stops when posts older than 12 hours are reached
         # if submission.id in database:
-            # break
+        # break
 
         minutes_since_post = (time.time() - submission.created_utc) / 60
-        if minutes_since_post > 720: # checks post is within past 12 hours
+        if minutes_since_post > 720:  # checks post is within past 12 hours
+            pass
             # db.remove(previous_post_older_than_12 hours)
             # db.add(submission.id)
         else:
-            if submission.score >= getenv("MIN_SUBMISSION_SCORE"):
+            if submission.score >= getenv("DPB_MINSCORE"):
                 # stickied discord promo comment
-                comment_made = submission.reply(getenv("STICKIED_MESSAGE"))
+                comment_made = submission.reply(getenv("DPB_MESSAGE"))
                 comment_made.mod.distinguish(sticky=True)
